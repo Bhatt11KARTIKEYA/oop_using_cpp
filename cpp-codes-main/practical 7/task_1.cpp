c2229b55fdfd6f3cd062254c0f6fabbd55d8350a
kartikeya Bhatt <90818254+Bhatt11KARTIKEYA@users.noreply.github.com>
2021-10-14 09:24:55 +0530

    Add files via upload

---
+++ cpp-codes-main/practical 7/task_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"Press 1 for  year \n press 2 for month \n press 3 for Day\n";
+cin>>n;
+
+switch(n)
+{
+case 1:
+cout<<"the year is : 2021"<<endl;
+break;
+
+case 2:
+cout<<"the month is october"<<endl;
+break;
+
+case 3:
+cout<<"the day is monday"<<endl;
+break;
+
+}
+
+
+return 0;
+}
